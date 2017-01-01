b2a9f20b00f7520d086267a68c948d9d091a113a
Xon <tazipper1@hotmail.com>
2017-01-01 12:10:15 +0800

    Prevent bots from accessing the 'order' argument for forums

---
+++ upload/library/SV/BotThreadView/XenForo/ControllerPublic/Forum.php
@@ -0,0 +1,20 @@
+<?php
+
+class SV_BotThreadView_XenForo_ControllerPublic_Forum extends XFCP_SV_BotThreadView_XenForo_ControllerPublic_Forum
+{
+    protected function _getDisplayConditions(array $forum)
+    {
+        if (SV_BotThreadView_Application::isBot())
+        {
+            list($defaultOrder, $defaultOrderDirection) = $this->_getDefaultThreadSort($forum);
+
+            $order = $this->_input->filterSingle('order', XenForo_Input::STRING, array('default' => $defaultOrder));
+            $orderDirection = $this->_input->filterSingle('direction', XenForo_Input::STRING, array('default' => $defaultOrderDirection));
+            if ($defaultOrder != $order || $orderDirection != $defaultOrderDirection)
+            {
+                throw $this->getNoPermissionResponseException();
+            }
+        }
+        return parent::_getDisplayConditions($forum);
+    }
+}
